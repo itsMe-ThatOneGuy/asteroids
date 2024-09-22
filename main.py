7eb1ade5d3dc42abae58ff5b505c1e4163ba4a4b
Matthew Smart <matthewsmart13@gmail.com>
2024-09-21 22:55:07 -0500

    Imported constants modules

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     print("Starting asteroids!")
 
 if __name__ == "__main__":
