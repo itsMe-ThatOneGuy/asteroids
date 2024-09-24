a0487dcac6bdb5b6b5e824f1102426e9c3553f39
Matthew Smart <matthewsmart13@gmail.com>
2024-09-24 00:26:34 -0500

    imported player module and added player

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     
     while True:
         for event in pygame.event.get():
@@ -13,6 +17,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
